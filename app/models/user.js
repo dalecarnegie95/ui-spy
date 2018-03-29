1f247a880f01f0ef5112566de3a33c1a62e33b92
Alisson Steffens <alisson.steffens@hotmail.com>
2018-03-29 12:05:48 -0300

    :gem: add last use

--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,8 @@ const UserSchema = {
   user: String,
   operational_system: String,
   is_online: Boolean,
-  portugol_version: String
+  portugol_version: String,
+  last_use: String
 }
 
 const User = mongoose.model('User', UserSchema)
